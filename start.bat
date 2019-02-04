1d8cb813bb04de1d909fddaa0d28a058be338611
Jcav <825195983@qq.com>
2019-02-04 20:19:27 +0800

    I forget this one.

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 
-java -jar backup.jar -delay=1000 -b -url="https://www.bilibili.com/video/av107" -month -st=2018-01 -ed=2018-12 -cookie=cookies.json
+java -jar backup.jar -delay=1000 -b -url="https://www.bilibili.com/video/av107" -history -st=2018-01 -ed=2018-12 -cookie=cookies.json
 
 
 
